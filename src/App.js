a2d71da58cf1ea1cfe51d5ed725f83ec3ca1642a
shaktawat19 <harshshaktawat.tech@gmail.com>
2023-09-07 11:55:43 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import kendoka from './kendoka.svg';
-import './App.scss';
+import React from 'react'
 
-function App() {
-  const handleClick = React.useCallback(() => {
-    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
-  }, []);
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={kendoka} className="App-logo" alt="kendoka" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button
-          themeColor={'primary'}
-          size={"large"}
-          onClick={handleClick}
-        >
-          Learn KendoReact
-        </Button>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
 export default App;
